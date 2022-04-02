245d843821dfa9da569a841fd6b6e541649ea310
chandana Reddy <chandanareshma01@gmail.com>
2022-04-02 16:06:57 +0530

    Validate last Name

--- Registration.cs
+++ Registration.cs
@@ -25,8 +25,8 @@ namespace UserRegistration
             {
                 Console.WriteLine(e.Message);
             }
+
         }
-      
         public void validateLastName(string lastName)
         {
             string stringForLastName = "^[A-Z][a-z]{3,}?";
@@ -44,88 +44,7 @@ namespace UserRegistration
                 Console.WriteLine(e.Message);
             }
         }
-       
-        public void validateEmailId(string emailID)
-        {
-            string stringForEmailId = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+.)+[a-z]{2,5}$";
-            try
-            {
-
-                if (Regex.IsMatch(emailID, stringForEmailId))
-                    Console.WriteLine(emailID + " is Valid");
-                else
-                    Console.WriteLine(emailID + " is Invalid");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-      
-        public void validateMobileNumber(string mobileNumber)
-        {
-            string stringForMobileNumber = "^[0-9]{10}";
-            try
-            {
-
-
-                if (Regex.IsMatch(mobileNumber, stringForMobileNumber))
-                    Console.WriteLine(mobileNumber + " is Valid");
-                else
-                    Console.WriteLine(mobileNumber + " is Invalid");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-       
-        public void validatePassword(string password)
-        {
-            string stringForPassword = "^.{8,}?";
-            try
-            {
-                if (Regex.IsMatch(password, stringForPassword))
-                    Console.WriteLine(password + " is Valid");
-                else
-                    Console.WriteLine(password + " is Invalid");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-       
-        public void validateUperCasePassword(string ucPassword)
-        {
-            string stringForUCPassword = "^[A-Z]+.{8,}?";
-            try
-            {
-                if (Regex.IsMatch(ucPassword, stringForUCPassword))
-                    Console.WriteLine(ucPassword + " is Valid");
-                else
-                    Console.WriteLine(ucPassword + " is Invalid");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-       
-        public void validateNumericPassword(string NumericPassword)
-        {
-            try
-            {
-                string stringForNumericPassword = "^.{8,}?";
-                if (Regex.IsMatch(NumericPassword, stringForNumericPassword))
-                    Console.WriteLine(NumericPassword + " is Valid");
-                else
-                    Console.WriteLine(NumericPassword + " is Invalid");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
     }
 }
+      
+      

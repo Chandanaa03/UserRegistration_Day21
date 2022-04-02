5110a44d130d925206e15f84bc37e428547fc322
chandana Reddy <chandanareshma01@gmail.com>
2022-04-02 16:04:20 +0530

    Valid First Name

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration validateUser = new Registration();
+            validateUser.validateFirstName("Chandana");
+            Console.WriteLine("--------------------------------------");
+            validateUser.validateLastName("Reddy");
+            Console.WriteLine("--------------------------------------");
+            validateUser.validateEmailId("chandanareshma@gmail.com");
+            Console.WriteLine("---------------------------------------");
+            validateUser.validateMobileNumber("7893338629");
+            Console.WriteLine("---------------------------------------");
+            validateUser.validatePassword("chandu123");
+            Console.WriteLine("---------------------------------------");
+            validateUser.validateUperCasePassword("Chandu123");
+            Console.WriteLine("---------------------------------------");
+            validateUser.validateNumericPassword("Chandu@123");
+            Console.WriteLine("---------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
